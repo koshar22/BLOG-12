6d278e6780b8eff0678ac319878ef71db861d7b2
koshar22 <saififiza454@gmail.com>
2021-12-09 21:31:33 +0530

    BLOG 12

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 let bars = document.getElementById('bars')
+let body = document.getElementById('body')
 let links = document.getElementsByClassName('links')[0]
 let isOpen = false
 
 bars.addEventListener('click', () => {
     if(isOpen){
         links.style.top = '-100%';
+        body.style.backgroundColor = '#fff';
         isOpen = false;
     }
     else {
         links.style.top = '70px';
+        body.style.backgroundColor = '#eee';
         isOpen = true;
     }
 })
@@ -20,14 +23,32 @@ let isOPen = false
 more.addEventListener('click', () => {
     if(isOPen) {
         child1.style.left = '-100%';
+        body.style.backgroundColor = '#fff';
         more.style.backgroundColor = '#53BD95';
         more.style.color = '#fff';
         isOPen = false;
     }
     else {
         child1.style.left = '0px';
-        more.style.backgroundColor = '#fff';
+        body.style.backgroundColor = '#eee';
+        more.style.backgroundColor = '#eee';
         more.style.color = '#53BD95';
         isOPen = true;
     }
 })
+
+
+// let bars = document.getElementById('bars')
+// let body = document.getElementById('body')
+// let links = document.getElementsByClassName('links')[0]
+
+
+// bars.addEventListener('click', () => {
+//     links.style.top = '70px';
+//     body.style.backgroundColor = '#eee';
+// })
+
+// body.addEventListener('click', () => {
+//     links.style.top = '-100%';
+//     body.style.backgroundColor = '#fff';
+// })
